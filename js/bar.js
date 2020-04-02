f5d8289fd7f30e2ffaf24782bc9c8ef42664352c
Luna-Lovegoood <49364660+Luna-Lovegoood@users.noreply.github.com>
2020-04-03 01:35:06 +0800

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -1,8 +1,9 @@
 var svg2 = d3.select("#chart")
 	.append("svg")
-	.attr("height", 1200)
+	.attr("height", 1130)
 	.attr("width", 600)
-	.style("border", "solid 1px #000");
+	//.style("border", "solid 1px #000")
+	.style("margin", "20px 0 0 0");
 
 d3.csv("count.csv").then(data => {
 	console.log(data);
@@ -19,13 +20,25 @@ d3.csv("count.csv").then(data => {
 		.attr("x", 280)
 		.attr("y", (d,i) => 20*i+50)
 		.attr("width", d => {
-			console.log(lengthScale(+d.count))
 			return lengthScale(+d.count)-280
 		})
 		.attr("height", 12)
 		.attr("fill", "lightblue");
 
-	svg2.selectAll("text")
+	svg2.selectAll(".tick")
+		.data(data)
+		.enter()
+		.append("text")
+		.attr("x", d => {
+			return lengthScale(+d.count)+8
+		})
+		.attr("y", (d,i) => 20*i+60)
+		.text(d => d.count)
+		.style("font-size", 10)
+		.style("text-anchor", "start")
+		.attr("class", "tick")
+
+	svg2.selectAll(".crime")
 		.data(data)
 		.enter()
 		.append("text")
@@ -34,5 +47,13 @@ d3.csv("count.csv").then(data => {
 		.text(d => d.crime)
 		.style("font-size", 10)
 		.style("text-anchor", "end")
+		.attr("class", "crime")
 		//.attr("fill", "lightblue");
+
+	svg2.append("text")
+        .style("font-size", 16)
+        .style("text-anchor", "middle")
+        .attr("x", 300)
+        .attr("y", 30)
+        .text("Number");
 })
